36277823995447944bb7934887c901e817eb081d
Brucearmstrong007 <brucearmstrongofficial@gmail.com>
2023-03-05 16:13:56 +0530

    Testing

--- libs/shared/cypress.config.ts
+++ libs/shared/cypress.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "cypress";
-import { nxComponentTestingPreset } from "@nrwl/angular/plugins/component-testing";
+import { defineConfig } from 'cypress';
+import { nxComponentTestingPreset } from '@nrwl/angular/plugins/component-testing';
 
 export default defineConfig({
   component: {
     ...nxComponentTestingPreset(__filename),
     specPattern:[
-      'components/src/**/*.cy.ts'
+      'component/src/**/*.cy.ts'
     ]
-    }
+  }
 });
